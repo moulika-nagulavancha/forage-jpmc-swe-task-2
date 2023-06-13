62a7a382d409356195d36f2a0230c07b18123c3f
moulika-nagulavancha <moulika152@gmail.com>
2023-06-13 12:27:33 -0500

    Final Revised Changes

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,6 +49,18 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+
+      /* adding attribute to Perspective-viewer elements*/
+      /*set view to y_line*/
+      elem.setAttribute('view','y_line');
+      /* set column-pivots to stock (ABC or DEF)*/
+      elem.setAttribute('column-pivots','["stock"]');
+       /* set row-pivots to timestamp*/
+      elem.setAttribute('row-pivots','["timestamp"]');
+     /* set columns to top_ask_price */
+      elem.setAttribute('columns','["top_ask_price"]');
+      /*aggregates to avoid duplicate data*/
+       elem.setAttribute('aggregates','{"stock":"distinct count","top_ask_price":"avg","top_bid_price": "avg","timestamp": "distinct count"}');
     }
   }
 
